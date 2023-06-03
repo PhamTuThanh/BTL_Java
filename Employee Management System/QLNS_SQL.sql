b7bbebf14e55f6f492dd264a5058391b6c9e9dc2
Pham Thanh <phamtuthanh0410@gmail.com>
2023-06-03 14:45:27 +0700

    add

---
+++ Employee Management System/QLNS_SQL.sql
@@ -0,0 +1,31 @@
+-- create database qlns;
+-- create table login(
+--   username varchar(50),
+--   password varchar(50)
+-- );
+insert into login values('tan', '1');
+create table qlnv(
+    name varchar(50),
+    surname varchar(50),
+    dob varchar(50),
+    salary varchar(50),
+    address varchar(50),
+    phone varchar(50),
+    email varchar(50),
+    education varchar(50),
+    gender varchar(50),
+    aadhar varchar(50),
+    empId varchar(50)
+)
+-- INSERT INTO qlnv(name, surname, dob, salary, address, phone, email, education, gender, aadhar, empId) 
+-- VALUES
+-- ('John', 'Doe', '1990-01-01', 5000, '123 Main St, Anytown, USA', '123-456-7890', 'johndoe@example.com', 'ABC123', 'Male', 'ID12345', 1);
+-- ('Jane', 'Doe', '1995-05-15', 6000, '456 Main St, Anytown, USA', '111-222-3333', 'janedoe@example.com', 'XYZ789','Female', 'ID67890', 2),
+-- ('James', 'Smith', '1988-03-20', 4500,'789 Elm St, Anytown, USA', '222-333-4444', 'jamessmith@example.com', 'DEF456', 'Male', 'ID24680', 3),
+-- ('Emily', 'Johnson', '1992-11-05', 5500, '987 Oak St, Anytown, USA', '444-555-6666', 'emilyjohnson@example.com', 'GHI789', 'Female', 'ID13579', 4),
+-- ('Michael', 'Brown', '1993-07-10', 5200, '543 Pine St, Anytown, USA', '777-888-9999', 'michaelbrown@example.com', 'JKL012', 'Male', 'ID24680', 5),
+-- ('Jessica', 'Garcia', '1985-12-25', 7000, '321 Cedar St, Anytown, USA', '555-666-7777', 'jessicagarcia@example.com', 'MNO345', 'Female', 'ID13579', 6),
+-- ('David', 'Martinez', '1989-09-15', 4800, '654 Maple St, Anytown, USA', '888-999-0000', 'davidmartinez@example.com', 'PQR678', 'Male', 'ID24680', 7),
+-- ('Samantha', 'Kim', '1996-04-30',5800, '789 Walnut St, Anytown, USA', '111-222-3333', 'samanthakim@example.com', 'STU901', 'Female', 'ID13579', 8),
+-- ('Christopher', 'Lee', '1991-08-12', 6500, '246 Oak St, Anytown, USA', '444-555-6666', 'christopherlee@example.com', 'VWX234', 'Male', 'ID24680', 9),
+-- ('Amanda', 'Nguyen', '1994-02-14', 6200, '135 Elm St, Anytown, USA', '777-888-9999', 'amandanguyen@example.com', 'YZA567', 'Female', 'ID13579', 10);
